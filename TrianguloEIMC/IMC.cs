cebeebf47dcb3cf4beafc8e70c21473466f5dcf0
Mandstoni <mdstoni@gmail.com>
2021-03-08 18:42:45 -0300

    Construcao do triangulo e imc

---
+++ TrianguloEIMC/IMC.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloEIMC
+{
+    class IMC
+    {
+        static void Main(string[] args) {
+            double peso, altura, imc;
+            int[] tableIMC ={18, 25, 30, 35, 40};
+
+            Console.Write("Informe seu peso em kg:");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe sua altura em metros (exemplo:1,57");
+            altura = double.Parse(Console.ReadLine());
+
+            imc = (peso / (altura * altura));
+
+            Console.Write("Seu IMC é: " + imc + "\n");
+
+
+            if (imc <= tableIMC[0])
+            {
+                Console.Write("Peso baixo");
+            }
+            else if (imc > tableIMC[0] && imc <= tableIMC[1])
+            {
+                Console.Write("Peso normal");
+            }
+            else if (imc > tableIMC[1] && imc <= tableIMC[2])
+            {
+                Console.Write("Sobrepeso");
+            }
+            else if (imc > tableIMC[2] && imc <= tableIMC[3])
+            {
+                Console.Write("Obesidade");
+            }
+            else if (imc > tableIMC[3] && imc <= tableIMC[4])
+            {
+                Console.Write("Obesidade severa");
+            }
+            else if (imc > tableIMC[4])
+            {
+                Console.Write("Obesidade mórbida");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ TrianguloEIMC/Triangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloEIMC
+{
+    class Triangulo
+    {
+        static void Main(string[] args)
+        {
+            int[] ladosTriangulo = new int[3];
+
+            for (int i = 0; i < ladosTriangulo.Length; i++)
+            {
+                Console.Write("informe o " + (i + 1) + " lado do triangulo:");
+                ladosTriangulo[i] = int.Parse(Console.ReadLine());
+            }
+
+            if (ladosTriangulo[0] == ladosTriangulo[1] && ladosTriangulo[1] == ladosTriangulo[2])
+            {
+                Console.Write("\nÉ um triangulo Equilátero");
+            }
+            else if ((ladosTriangulo[0] == ladosTriangulo[1]) || (ladosTriangulo[1] == ladosTriangulo[2]) ||
+               (ladosTriangulo[0] == ladosTriangulo[2]))
+            {
+                Console.Write("\nÉ um triângulo Isósceles");
+            }
+            else
+            {
+                Console.Write("\nÉ um triângulo Escaleno");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
